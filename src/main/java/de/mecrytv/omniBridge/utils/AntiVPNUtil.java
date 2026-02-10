81e2e6c460339d2c1b7c480a4be89404edef079e
MecryTv <Development@mecrytv.de>
2026-02-10 23:02:59 +0100

    [ADD] AntiVPNUtil

---
+++ src/main/java/de/mecrytv/omniBridge/utils/AntiVPNUtil.java
@@ -0,0 +1,42 @@
+package de.mecrytv.omniBridge.utils;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import de.mecrytv.omniBridge.OmniBridge;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
+
+public class AntiVPNUtil {
+
+    private static final HttpClient httpClient = HttpClient.newHttpClient();
+
+    public static CompletableFuture<Boolean> isVPN(String ip){
+        String apiKey = OmniBridge.getInstance().getConfig().getString("vpnAPIKey");
+        String url = "https://proxycheck.io/v2/" + ip + "?key=" + apiKey + "&vpn=1";
+
+        if (apiKey.isEmpty()) return CompletableFuture.completedFuture(false);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+
+        return httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString()).thenApply(response -> {
+                   if (response.statusCode() == 200) {
+                       JsonObject json = JsonParser.parseString(response.body()).getAsJsonObject();
+
+                       if (json.has(ip)) {
+                           JsonObject ipData = json.getAsJsonObject(ip);
+                           return ipData.has("proxy") && ipData.get("proxy").getAsString().equalsIgnoreCase("yes");
+                       }
+                   }
+                   return false;
+                }).exceptionally(ex -> {
+                    ex.printStackTrace();
+                    return false;
+        });
+    }
+}
